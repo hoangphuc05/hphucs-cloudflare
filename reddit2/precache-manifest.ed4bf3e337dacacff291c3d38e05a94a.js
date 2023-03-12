df686bf85b23559cdaa93691ee015e674ce5e265
Phuc Hoang <chphuc05@gmail.com>
2023-03-11 17:44:19 -0800

    add rvn2 page

---
+++ reddit2/precache-manifest.ed4bf3e337dacacff291c3d38e05a94a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f014a1d734f6824672f9a2d70478f387",
+    "url": "/reddit2/index.html"
+  },
+  {
+    "revision": "fa8e86e4b81581f0310c",
+    "url": "/reddit2/static/css/main.a6b53d02.chunk.css"
+  },
+  {
+    "revision": "a381a362016c7d7c68a8",
+    "url": "/reddit2/static/js/2.3cbaa982.chunk.js"
+  },
+  {
+    "revision": "727196b53be8768c78b2ea60f81f0ea1",
+    "url": "/reddit2/static/js/2.3cbaa982.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa8e86e4b81581f0310c",
+    "url": "/reddit2/static/js/main.65f6590c.chunk.js"
+  },
+  {
+    "revision": "9efe33853ced5fe34ddf",
+    "url": "/reddit2/static/js/runtime-main.d086c75e.js"
+  }
+]);
